6557db4dcfb0b8af2c45afa43382c8c30a7c0567
Surjeet Kumar <115608471+Surjeet4146@users.noreply.github.com>
2025-06-04 17:25:47 +0530

    Create utils/Constants.java

---
+++ app/src/main/java/com/studentdashboard/utils/Constants.java
@@ -0,0 +1,14 @@
+```java
+package com.studentdashboard.utils;
+
+public class Constants {
+    public static final String STUDENTS_NODE = "students";
+    public static final String COURSES_NODE = "courses";
+    public static final String USER_PREFS = "user_preferences";
+    
+    // Error messages
+    public static final String ERROR_NETWORK = "Network error. Please check your connection.";
+    public static final String ERROR_AUTH = "Authentication failed. Please try again.";
+    public static final String ERROR_GENERIC = "Something went wrong. Please try again.";
+}
+```
